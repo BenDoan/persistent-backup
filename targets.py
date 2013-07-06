727d6927c44fa590eda3ac9741fa4f8ff9fcc492
Ben Doan <ben@bendoan.me>
2013-07-06 16:02:48 -0500

    added targets.py

---
+++ targets.py
@@ -0,0 +1,14 @@
+targets = [
+    {
+        'user':'ben',
+        'ip':'192.168.5.65',
+        'destination':"/home/ben/code/persistant-backup/backup/",
+        'source':"/home/ben/code/persistant-backup/docs"
+    },
+    {
+        'user':'ben',
+        'ip':'192.168.1.4',
+        'destination':"/home/ben/code/persistant-backup/backup/",
+        'source':"/home/ben/code/persistant-backup/docs"
+    }
+]
