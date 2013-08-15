ca24a11f75946167cb1d43c3f721d83ba099e3ac
Ben Doan <ben@bendoan.me>
2013-08-15 13:47:36 -0500

    removed logging from rsync lib

--- RSyncBackup.py
+++ RSyncBackup.py
@@ -34,7 +34,6 @@
 __version__ = "1.3"
 
 import time, subprocess, re
-import logging
 import os, os.path
 
 class RSyncBackup:
@@ -49,12 +48,9 @@ class RSyncBackup:
             rsync       - Specify the location of the rsync binary
             testRun     - Set to 1 to log out what will be done, rather than doing it.
         """
-        self.log = logging.getLogger ("RSyncBackup")
         self.lastRunFile = lastRunFile
         self.rsync = rsync
         self.testRun = testRun
-        if (testRun):
-            self.log.info ("TestRun detected - no file operations will be performed.")
         self.backupStarted = time.gmtime()
 
     def timeToBackup (self, backupInterval=24*60):
@@ -68,22 +64,17 @@ class RSyncBackup:
             oldTime = lrf.read()
             lrf.close()
         except Exception as e:
-            self.log.warn ("Exception occured reading the last run file %s.  Error: %s" % (self.lastRunFile, str (e)))
             self.backupStarted = time.gmtime()
             return 1
         try:
             lastTime = time.mktime (time.strptime (oldTime))
             nowTime = time.mktime (time.gmtime())
-            self.log.debug ("lastTime: %s nowTime: %s" % (str (lastTime), str (nowTime)))
             if (lastTime + backupInterval * 60 < nowTime):
-                self.log.info ("Time to perform backup.")
                 self.backupStarted = time.gmtime()
                 return 1
             else:
-                self.log.info ("Not yet time to backup.")
                 return 0
         except Exception as e:
-            self.log.warn ("Exception occured parsing last run file %s.  Error: %s" % (self.lastRunFile, str (e)))
             self.backupStarted = time.gmtime()
             return 1
 
@@ -98,15 +89,9 @@ class RSyncBackup:
 
             Returns true if successful, false if an error occurs.
         """
-        if (self.testRun):
-            self.log.info ("Starting test run of backup.")
-        else:
-            self.log.info ("Starting backup.")
         dateTime = time.strftime ("%d%m%Y-%H%M%S")
         if (archive is not None):
-            self.log.debug ("Determining archive directory.")
             thisArchive = os.path.join (archive, dateTime[4:8], dateTime[2:4], dateTime)
-            self.log.debug ("Archive destination is: %s" % thisArchive)
         else:
             thisArchive = None
 
@@ -118,17 +103,12 @@ class RSyncBackup:
             for exclude in excludeList:
                 cmnd = '%s --exclude="%s"' % (cmnd, exclude)
         cmnd = "%s '%s' '%s'" % (cmnd, source, destination)
-        self.log.debug ("Running command: %s" % cmnd)
         if (self.testRun):
-            self.log.warn ("TestRun - would execute command: " + cmnd)
+            pass
         else:
             result = subprocess.getstatusoutput (cmnd)
             if (result[0]  != 0):
-                self.log.error ("Error running rsync: %s" % result[1])
                 return 0
-            else:
-                self.log.info ("Rsync backup successful.")
-                self.log.debug ("Rsync output: %s" % result[1])
             if debug:
                 print("RSync Output:\n")
                 print(result[1])
@@ -150,7 +130,6 @@ class RSyncBackup:
             # Default to triming the total number of archives.
             # Filter on archiveDir/yyyy/dd/ddmmyyyy-hhmmss
             filterStr = os.path.join (re.escape (archiveDir), '[0-9]{4,4}', '[0-9]{2,2}', '[0-9]{8,8}-[0-9]{6,6}$')
-            self.log.debug ("Using trim filter of: %s" % filterStr)
             nameFilter = re.compile (filterStr)
         else:
             nameFilter = re.compile (filter)
@@ -163,54 +142,42 @@ class RSyncBackup:
         pathKiller = pathRemover ()
         for pathToRemove in pathsToTrim:
             if (self.testRun):
-                self.log.warn ("TestRun - would remove path: " + pathToRemove)
+                pass
             else:
                 if (os.path.isdir (pathToRemove)):
-                    self.log.info ("Recursively deleting all files in %s" % pathToRemove)
                     pathKiller.walk (pathToRemove)
-                    self.log.info ("Removing directory %s" % pathToRemove)
                     os.rmdir (pathToRemove)
                 else:
-                    self.log.error ("Removing file %s" % pathToRemove)
                     os.remove (pathToRemove)
             if (removeParentIfEmpty):
-                self.log.debug ("Checking to see whether parent directories are empty")
                 lastParent = pathToRemove
                 looking = 1
                 while (looking):
                     parent = os.path.split (lastParent)[0]
                     if (lastParent == parent):
-                        self.log.info ("Reach top of hierachy when looking for parents to delete.")
                         looking = 0
                     else:
                         if (len (os.listdir (parent)) == 0):
                             if (self.testRun):
-                                self.log.warn ("TestRun - would remove path: " + parent)
+                                pass
                             else:
-                                self.log.info ("Found that parent directory %s is empty - deleting." % parent)
                                 os.rmdir (parent)
                             # We are going to carry on looking, so note this as the last parent
                             lastParent = parent
                         else:
-                            self.log.debug ("Parent directory is not empty - preserving.")
                             looking = 0
 
     def finish (self):
         """ Write out the time the backup started to the last run file (if one used). """
         if (self.testRun):
-            self.log.debug ("TestRun - not writing out last run file.")
             return
         if (self.lastRunFile is None):
-            self.log.debug ("Last Run File not given, skipping writing.")
             return
         try:
-            self.log.debug ("Time backup was started: %s" % time.asctime (self.backupStarted))
             lrf = open (self.lastRunFile, 'w')
             lrf.write (time.asctime (self.backupStarted))
             lrf.close()
-            self.log.info ("Last Run File updated succesfully")
         except Exception as e:
-            self.log.error ("Exception occured writing the last run file %s.  Error: %s" % (self.lastRunFile, str (e)))
             raise Exception ("Error writing to last run file!")
 
 class pathWalker:
@@ -230,7 +197,6 @@ class pathWalker:
 class pathRemover:
     def __init__ (self):
         self.dirsToRemove = []
-        self.log = logging.getLogger ("RSyncBackup.pathRemover")
 
     def walk (self, path):
         os.path.walk (path, self.walking, None)
@@ -242,7 +208,6 @@ class pathRemover:
 
     def walking (self, arg, dirname, names):
         for name in names:
-            #self.log.debug ("Would delete file: %s" % os.path.join (dirname, name))
             target = os.path.join (dirname, name)
             if (os.path.isdir (target)):
                 self.dirsToRemove.append (target)
